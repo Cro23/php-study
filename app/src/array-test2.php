69954e6bdac0d17a640bd2f8cf07ba5b155b4651
yutaronakashima <nakashima@iaiweb.co.jp>
2023-05-23 11:57:18 +0900

    配列を合成する2つの方法 「加算演算子(+)、array_merge関数」

---
+++ app/src/array-test2.php
@@ -0,0 +1,41 @@
+<?php
+// 配列を合成する2つの方法 -- 加算の演算子(+)とarray_merge関数
+// 以下は、同じ配列を、+記号とarray_merge関数を使って合成する例。合成対象の22つの配列が同一キーを持っていた時の、細かな挙動の違いも確認
+
+  // 配列を合成
+  $arr1 = ['a','b'];
+  $arr2 = ['d','e','f'];
+  $mergedA = $arr1 + $arr2;
+  $mergedB = $arr2 + $arr1;
+  $mergedC = array_merge($arr1, $arr2);
+  $mergedD = array_merge($arr2, $arr1);
+  print_r($mergedA);
+  print_r($mergedB);
+  print_r($mergedC);
+  print_r($mergedD);
+
+
+  // 連想配列を合成
+
+  $arr3 = [
+    'a' => 'value A',
+    'b' => 'value B',
+  ];
+  $arr4 = [
+    'a' => 'value C',
+    'b' => 'value D',
+    'c' => 'value E',
+  ];
+
+  $mergedE = $arr3 + $arr4;
+  $mergedF = $arr4 + $arr3;
+  $mergedG = array_merge($arr3, $arr4);
+  $mergedH = array_merge($arr4, $arr3);
+
+  print_r($mergedE);
+  print_r($mergedF);
+  print_r($mergedG);
+  print_r($mergedH);
+
+
+?>
