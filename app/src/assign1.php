63e515cdc2b93240179a81ea728e5b967ad59572
yutaronakashima <nakashima@iaiweb.co.jp>
2023-05-23 12:40:25 +0900

    すでにある変数値に演算を加える 「複合演算子」

---
+++ app/src/assign1.php
@@ -0,0 +1,12 @@
+<?php
+// 既にある変数値に演算を加える -- 複合演算子
+
+  $sum = 3;
+  $sum *= 5; // $sum = $sum * 5; と同じ意味
+
+  $greeting = 'Hello ';
+  $greeting .= 'World.'; // $greeting = $greeting . 'World';と同じ意味
+
+  echo 'sum：',$sum;
+  echo 'greeting：',$greeting;
+?>
