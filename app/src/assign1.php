bd66e1905cc52ccb5b06bcd31d83b10541603964
yutaronakashima <nakashima@iaiweb.co.jp>
2023-05-23 12:50:18 +0900

    既にある変数値に演算を加える 「複合演算子」

--- app/src/assign1.php
+++ app/src/assign1.php
@@ -9,4 +9,14 @@
 
   echo 'sum：',$sum;
   echo 'greeting：',$greeting;
+
+
+// 2つの変数で1つの値を共有する -- リファレンス渡し
+// $b = &$a
+  $greeting1 = 'Hello';
+  $greeting2 = &$greeting1;
+  $greeting1 = 'World';
+
+  echo 'greeting1：',$greeting1;
+  echo 'greeting2：',$greeting2;
 ?>
