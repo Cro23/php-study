b15ba172fc76d317813e325fb4016682006db5fb
yutaronakashima <nakashima@iaiweb.co.jp>
2023-05-23 11:03:19 +0900

    配列の各要素をスカラー型変数に割り当てる

---
+++ app/src/array-test.php
@@ -0,0 +1,15 @@
+<?php
+// 配列の各要素をスカラー型変数に割り当てる -- list()
+$userData = [12345,'Taro Yamada','TOKYO',32];
+list($id,$name,$prefecture,$age) = $userData;
+print_r($userData);
+?>
+
+<p><?php echo $id ?></p>
+<p><?php echo $name ?></p>
+<p><?php echo $prefecture ?></p>
+<p><?php echo $age ?></p>
+
+<!-- list()を使う必要があるのは、CSVファイルの1列目を「ユーザーID」、2列目を「ユーザ名」と解釈させるというように、
+元々は順序で管理されていたデータに人間的な意味を与えたい時です。
+プログラマー自身がデータ構造を定義できる時は、はじめから連想配列で定義した方がスマートなプログラムになります。 -->
