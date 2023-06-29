cdf269c5398d755328a4c57b0a9b1842e0faf054
yutaronakashima <nakashima@iaiweb.co.jp>
2023-06-29 16:27:09 +0900

    データ型の暗黙的な変換を防ぐ -- declare文とstrict_types

--- app/src/declare.php
+++ app/src/declare.php
@@ -12,6 +12,9 @@
 
 // 例
 // function doSomething(int $num, string $message): bool
+// {
+//   return true;
+// }
 //
 // この時、それぞれの引数や戻り値は、nullであってはなりません。
 
@@ -21,3 +24,33 @@
 //
 // nullを引数として受け取ったり、nullを戻り値として返すことを許容したいときは、
 // データ型名の前にクエスチョン記号（?）をつけます
+
+/**
+ * 
+ * データ型の暗黙的な変換を防ぐ -- declare文とstrict_types
+ * 
+ * 型宣言を行なっても、期待していないデータ型が渡せてしまう場合があります。
+ * 
+ * 
+ */
+
+//  $ret = doSomething(true, 'ARG2');
+ // 第1引数が整数型の1に変換され、エラーとならない
+
+//  $ret = doSomething('3', 'ARG2'); 
+ // 第1引数が整数型の3に変換され、エラーとならない
+
+//  ↑PHPが、データ型を暗黙的に変換した上で関数に渡しているからです。
+
+//  この暗黙的な型変換を防ぎ、エラーを発生させたい時は、プログラムファイルの冒頭で
+//  declare命令を使い、厳密な型チェック（strict types）を有効にする必要があります。
+
+declare(strict_types=1);
+
+function doSomething(int $num, string $message): bool
+{
+  return true;
+}
+
+$ret = doSomething(true, 'ARG2'); //ここでエラーとなり、プログラム処理が終了する
+$ret = doSomething('3', 'ARG2'); //実行されない
