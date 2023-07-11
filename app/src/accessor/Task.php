b003c7f0865ed57042f6640e495bcbe1cfb69c16
yutaronakashima <nakashima@iaiweb.co.jp>
2023-07-11 13:50:24 +0900

    プロパティはprivateが基本 -- アクセサメソッド

---
+++ app/src/accessor/Task.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+class Task
+{
+  // タスク名
+  private string $name;
+
+  // 進行度（0~100で表す）
+  private int $progress;
+
+  // 外に知られたくない秘密のトークン値
+  private $secret;
+
+  // nameプロパティのゲッターメソッド
+  public function getName(): string
+  {
+    return $this->name;
+  }
+
+  // nameプロパティのセッターメソッド
+  public function setName(string $name): void
+  {
+    $this->name = $name;
+  }
+
+  // progressプロパティのゲッターメソッド
+  public function getProgress(): int
+  {
+    return $this->progress;
+  }
+
+  // progressプロパティのセッターメソッド
+  public function setProgress(int $progress): void
+  {
+    if ($progress < 0) {
+      $progress = 0;
+    } elseif ($progress > 100) {
+      $progress = 100;
+    }
+    $this->progress = $progress;
+  }
+
+  // タスクを完了ステータスにする
+  public function completeProgress(): void {
+    $this->progress = 100;
+  }
+
+  // タスクを未完了ステータスにする
+  public function clearProgress(): void {
+    $this->progress = 0;
+  }
+}
