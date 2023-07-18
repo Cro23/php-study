5d1b33cc2dbf8795640fa102312aed9d5034a3ab
yutaronakashima <nakashima@iaiweb.co.jp>
2023-07-18 10:44:52 +0900

    名前空間に所属するクラスを呼び出す

---
+++ app/src/namespace/office/word/Writer.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Office\Word;
+
+// Wordファイルの書き出しクラス
+class Writer
+{
+  // Wordファイルに書き出すメソッド
+  public function write(): void
+  {
+    echo 'Wordファイルを書き出します。', PHP_EOL;
+  }
+}
